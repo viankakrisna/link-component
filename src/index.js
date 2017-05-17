c5212a471b2db67cbe6b656f749feea74a05eb0c
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-05-18 01:27:12 +0700

    tweak onClick event

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from "react";
-import history from "./history";
+import React from 'react';
+export history from './history';
 
 export default function Link({ to, ...props }) {
   return (
     <a
       href={to}
+      {...props}
       onClick={e => {
         e.preventDefault();
         history.push(to);
+        if (typeof props.onClick === 'function'){
+          props.onClick()
+        }
       }}
-      {...props}
     />
   );
 }
